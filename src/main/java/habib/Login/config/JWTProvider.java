035965d1ba5d3e1ecba0a8d3be1f14e1358e6c98
Habib-Lotfi <129693455+Habib-Lotfi@users.noreply.github.com>
2024-04-28 22:12:49 -0400

    Update JWTProvider.java

--- src/main/java/habib/Login/config/JWTProvider.java
+++ src/main/java/habib/Login/config/JWTProvider.java
@@ -17,7 +17,7 @@ public class JWTProvider {
     public String generateToken(Authentication auth) {
 
         String jwt = Jwts.builder().setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime() - 86400000))
+                .setExpiration(new Date(new Date().getTime() + 86400000))
                 .claim("email", auth.getName())
                 .signWith(key).compact();
 
